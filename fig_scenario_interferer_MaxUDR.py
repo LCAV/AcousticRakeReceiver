82f082462609208d2226e26f4cbdb00ba98ddf24
Robin Scheibler <fakufaku@gmail.com>
2014-07-08 00:28:05 +0200

    Fixed four plots for the scenario plot of paper.

--- fig_scenario_interferer_MaxUDR.py
+++ fig_scenario_interferer_MaxUDR.py
@@ -10,23 +10,28 @@ import beamforming as bf
 import windows
 import utilities as u
 
+'''
+This script simulates a scenario where a single source
+of interest is present in a Room.
+'''
+
 # Some simulation parameters
 Fs = 8000
 t0 = 1./(Fs*np.pi*1e-2)  # starting time function of sinc decay in RIR response
 absorption = 0.90
 max_order_sim = 10
-sigma2_n = 1e-7
+sigma2_n = 1e-7          # Noise power (should replace with proper SNR)
 
 # Room 1 : Shoe box
 room_dim = [4, 6]
 
 # the sources
-source1 = [2.5, 4.5]    # good source
-source2 = [2.25, 3]     # interferer
+source1 = [1, 4.5]        # good source
+source2 = [3, 4]    # interferer
 
 # microphone array design parameters
 mic1 = [2, 1.5]         # position
-M = 8                    # number of microphones
+M = 8                   # number of microphones
 d = 0.08                # distance between microphones
 phi = 0.                # angle from horizontal
 max_order_design = 1    # maximum image generation used in design
@@ -35,13 +40,15 @@ shape = 'Linear'        # array shape
 # create a microphone array
 if shape is 'Circular':
     mics = bf.Beamformer.circular2D(Fs, mic1, M, phi, d*M/(2*np.pi)) 
+elif shape is 'Poisson':
+    mics = bf.Beamformer.poisson(Fs, mic1, M, d) 
 else:
     mics = bf.Beamformer.linear2D(Fs, mic1, M, phi, d) 
 
 # define the array processing type
-L = 8192                # frame length
-hop = 4096              # hop between frames
-zp = 4096               # zero padding (front + back)
+L = 4096                # frame length
+hop = 2048              # hop between frames
+zp = 2048               # zero padding (front + back)
 mics.setProcessing('FrequencyDomain', L, hop, zp, zp)
 
 # The first signal (of interest) is singing
@@ -72,25 +79,27 @@ room1.addSource(source2, signal=signal2, delay=delay2)
 room1.addMicrophoneArray(mics)
 
 # compute RIRs
-room1.compute_RIR()
+#room1.compute_RIR()
 
 # simulate the microphone signals
-room1.simulate()
+#room1.simulate()
 
 # Compute the beamforming weights depending on room geometry
 good_source = room1.sources[0].getImages(max_order=max_order_design)
 bad_source = room1.sources[1].getImages(max_order=max_order_design)
-mics.rakeMaxSINRWeights(good_source, bad_source, 
-                        R_n = sigma2_n*np.eye(mics.M), 
-                        rcond=0., 
-                        attn=True, ff=False)
+mics.rakeMaxUDRWeights(good_source, bad_source, 
+                         R_n = sigma2_n*np.eye(mics.M), 
+                         attn=True, ff=False)
+
+#I = np.abs(mics.weights) > 1
+#mics.weights[I] *= 1./np.abs(mics.weights[I])
 
 # process the signal through the beamformer
-output = mics.process()
+#output = mics.process()
 
 # save the array signal to file
-mics.to_wav('raw_output.wav', mono=True, norm=True, type=float)
-wavfile.write('proc_output.wav', Fs, u.normalize(output))
+#mics.to_wav('raw_output.wav', mono=True, norm=True, type=float)
+#wavfile.write('proc_output.wav', Fs, u.normalize(output))
 
 # plot the room and beamformer
 # make the picutre 6x10cm ~ 2.4x3.9in
@@ -99,17 +108,21 @@ fig, ax = room1.plot(img_order=np.minimum(room1.max_order, 1),
         figsize=(2.4,3.9), no_axis=True,
         xlim=[-4,8], ylim=[-8,12],
         autoscale_on=False)
-fig.savefig('figures/room_interferer_in_direct_path.pdf')
+fig.savefig('figures/scenario_interferer_MaxUDR.pdf')
 
 # Plot the TF of beamformer as seen from source and interferer
 plt.figure()
 mics.plot_response_from_point(np.array([source1, source2]).T, 
                               legend=('source','interferer'))
 
-# open and plot the two signals
+# Plot the weights in FD and TD
 plt.figure()
-u.comparePlot(mics.signals[mics.M/2], output, Fs, fft_size=400, 
-        norm=True, equal=True, title1='Mic input', title2='Beamformer output')
+mics.plot()
+
+# open and plot the two signals
+#plt.figure()
+#u.comparePlot(mics.signals[mics.M/2], output, Fs, fft_size=400, 
+        #norm=True, equal=True, title1='Mic input', title2='Beamformer output')
 
 # show all plots
 plt.show()
--- fig_scenario_no_interferer_MaxSINR.py
+++ fig_scenario_no_interferer_MaxSINR.py
@@ -10,16 +10,11 @@ import beamforming as bf
 import windows
 import utilities as u
 
-'''
-This script simulates a scenario where a single source
-of interest is present in a Room.
-'''
-
 # Some simulation parameters
 Fs = 8000
 t0 = 1./(Fs*np.pi*1e-2)  # starting time function of sinc decay in RIR response
-absorption = 0.90
-max_order_sim = 10
+absorption = 0.90        # absorption of walls
+max_order_sim = 10       # number of generations of image sources in simulation
 sigma2_n = 1e-7          # Noise power (should replace with proper SNR)
 
 # Room 1 : Shoe box
@@ -27,7 +22,6 @@ room_dim = [4, 6]
 
 # the sources
 source1 = [1, 4.5]        # good source
-source2 = [3, 4]    # interferer
 
 # microphone array design parameters
 mic1 = [2, 1.5]         # position
@@ -46,9 +40,9 @@ else:
     mics = bf.Beamformer.linear2D(Fs, mic1, M, phi, d) 
 
 # define the array processing type
-L = 4096                # frame length
-hop = 2048              # hop between frames
-zp = 2048               # zero padding (front + back)
+L = 8192                # frame length
+hop = 4096              # hop between frames
+zp = 4096               # zero padding (front + back)
 mics.setProcessing('FrequencyDomain', L, hop, zp, zp)
 
 # The first signal (of interest) is singing
@@ -57,13 +51,6 @@ signal1 = np.array(signal1, dtype=float)
 signal1 = u.normalize(signal1)
 signal1 = u.highpass(signal1, Fs)
 delay1 = 0.
-
-# the second signal (interferer) is some german speech
-rate2, signal2 = wavfile.read('samples/german_speech_'+str(Fs)+'.wav')
-signal2 = np.array(signal2, dtype=float)
-signal2 = u.normalize(signal2)
-signal2 = u.highpass(signal2, Fs)
-delay2 = 1.
 
 # create the room with sources and mics
 room1 = rg.Room.shoeBox2D(
@@ -75,32 +62,27 @@ room1 = rg.Room.shoeBox2D(
     absorption=absorption,
     sigma2_awgn=sigma2_n)
 room1.addSource(source1, signal=signal1, delay=delay1)
-room1.addSource(source2, signal=signal2, delay=delay2)
 room1.addMicrophoneArray(mics)
 
 # compute RIRs
-#room1.compute_RIR()
+room1.compute_RIR()
 
 # simulate the microphone signals
-#room1.simulate()
+room1.simulate()
 
 # Compute the beamforming weights depending on room geometry
 good_source = room1.sources[0].getImages(max_order=max_order_design)
-bad_source = room1.sources[1].getImages(max_order=max_order_design)
-mics.rakeMaxSINRWeights(good_source, bad_source, 
-                         R_n = sigma2_n*np.eye(mics.M), 
-                         rcond=0., 
-                         attn=True, ff=False)
-
-#I = np.abs(mics.weights) > 1
-#mics.weights[I] *= 1./np.abs(mics.weights[I])
+mics.rakeMaxSINRWeights(good_source, None, 
+                        R_n = sigma2_n*np.eye(mics.M), 
+                        rcond=0., 
+                        attn=True, ff=False)
 
 # process the signal through the beamformer
-#output = mics.process()
+output = mics.process()
 
 # save the array signal to file
-#mics.to_wav('raw_output.wav', mono=True, norm=True, type=float)
-#wavfile.write('proc_output.wav', Fs, u.normalize(output))
+mics.to_wav('raw_output.wav', mono=True, norm=True, type=float)
+wavfile.write('proc_output.wav', Fs, u.normalize(output))
 
 # plot the room and beamformer
 # make the picutre 6x10cm ~ 2.4x3.9in
@@ -109,21 +91,17 @@ fig, ax = room1.plot(img_order=np.minimum(room1.max_order, 1),
         figsize=(2.4,3.9), no_axis=True,
         xlim=[-4,8], ylim=[-8,12],
         autoscale_on=False)
-fig.savefig('figures/room_interferer.pdf')
+fig.savefig('figures/scenario_no_interferer_MaxSINR.pdf')
 
 # Plot the TF of beamformer as seen from source and interferer
 plt.figure()
-mics.plot_response_from_point(np.array([source1, source2]).T, 
+mics.plot_response_from_point(np.array([source1]).T, 
                               legend=('source','interferer'))
 
-# Plot the weights in FD and TD
-plt.figure()
-mics.plot()
-
 # open and plot the two signals
-#plt.figure()
-#u.comparePlot(mics.signals[mics.M/2], output, Fs, fft_size=400, 
-        #norm=True, equal=True, title1='Mic input', title2='Beamformer output')
+plt.figure()
+u.comparePlot(mics.signals[mics.M/2], output, Fs, fft_size=400, 
+        norm=True, equal=True, title1='Mic input', title2='Beamformer output')
 
 # show all plots
 plt.show()
