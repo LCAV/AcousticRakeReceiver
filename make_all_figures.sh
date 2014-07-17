77bbc012b6cd6626e1d7db6dadca1933246eb079
Robin Scheibler <fakufaku@gmail.com>
2014-07-17 09:06:41 +0200

    Forgotten to add the make_all_figures script.

---
+++ make_all_figures.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create all figures and sound samples
+
+ipython figure_spectrograms.py
+
+ipython figure_beam_scenarios.py
+
+ipython figure_Measures1.py
+
+ipython figure_Measures2.py
+
+ipython figure_SumNorm.py
+
