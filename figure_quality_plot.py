fc8af0bfd54eda9af8695bbced58ae3df1b10a57
Robin Scheibler <fakufaku@gmail.com>
2015-01-12 17:30:30 +0100

    figure_quality_plot now takes the name of the simulation result files as argument.

--- figure_quality_plot.py
+++ figure_quality_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import utilities as u
 import metrics as metrics
 
+import sys
 import os
 import fnmatch
 
@@ -12,17 +13,18 @@ sim_data_dir = './sim_data/'
 beamformer_names = ['Rake-DS',
                     'Rake-MaxSINR',
                     'Rake-MaxUDR']
+bf_dict = dict(zip(beamformer_names, 
+               range(len(beamformer_names))))
 NBF = len(beamformer_names)
 
 loops = 0
 
-# Specify all files to use with a pattern
-name_pattern = 'quality_2015*.npz'
-files = [file for file in os.listdir(sim_data_dir) if fnmatch.fnmatch(file, name_pattern)]
-
-# Or just specify a list of file names
-files = [ '20150109_10000Loops/quality_20150109-070951z_fixed.npz', 
-          '20150109_10000Loops/quality_20150109-095429z.npz', ]
+if len(sys.argv) == 0:
+    # if no argument is specified, use all available files
+    name_pattern = './sim_data/quality_2015*.npz'
+    files = [file for file in os.listdir(sim_data_dir) if fnmatch.fnmatch(file, name_pattern)]
+else:
+    files = sys.argv[1:]
 
 # Empty data containers
 good_source = np.zeros((0,2))
@@ -38,7 +40,7 @@ osinr_bf = np.zeros((0,NBF,max_sources))
 for fname in files:
     print 'Loading from',fname
 
-    a = np.load(sim_data_dir + fname)
+    a = np.load(fname)
 
     good_source = np.concatenate((good_source, a['good_source']), axis=0)
     bad_source = np.concatenate((bad_source, a['bad_source']), axis=0)
@@ -74,18 +76,28 @@ plt.figure()
 plt.plot(good_source[:,0], good_source[:,1], 'o')
 plt.plot(bad_source[:,0], bad_source[:,1], '*')
 
-plt.figure(figsize=(18,9))
+def nice_plot(x, ylabel, bf_order=None):
 
-newmap = plt.get_cmap('gist_heat')
-from itertools import cycle
-lines = ['-s','-o','-v','-D','->']
-linecycler = cycle(lines)
+    if bf_order is None:
+        bf_order = beamformer_names
 
-def nice_plot(x):
     ax1 = plt.gca()
-    ax1.set_color_cycle([newmap( k ) for k in np.linspace(0.25,0.9,len(beamformer_names))])
 
-    for i, bf in enumerate(beamformer_names):
+    newmap = plt.get_cmap('gist_heat')
+    from itertools import cycle
+
+    # totally a hack to get the same line styles as Fig6/7
+    lines = ['-D','-v','->','-s','-o']
+    linecycler = cycle(lines)
+
+    # totally a hack to get the same line styles as Fig6/7
+    map1 = [newmap( k ) for k in np.linspace(0.25,0.9,5)]
+    map2 = [map1[3],map1[2],map1[4],map1[0],map1[1]]
+
+    ax1.set_color_cycle(map2)
+
+    for bf in bf_order:
+        i = bf_dict[bf]
         p, = plt.plot(range(0, max_sources), 
                 np.median(x[:,i,:], axis=0),
             next(linecycler),
@@ -102,43 +114,69 @@ def nice_plot(x):
                 edgecolor='k',
                 alpha=0.7)
 
+        # Hide right and top axes
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['bottom'].set_position(('outward', 10))
+        ax1.spines['left'].set_position(('outward', 15))
+        ax1.yaxis.set_ticks_position('left')
+        ax1.xaxis.set_ticks_position('bottom')
+
+        # Make ticks nicer
+        ax1.xaxis.set_tick_params(width=.3, length=3)
+        ax1.yaxis.set_tick_params(width=.3, length=3)
+
+        # Make axis lines thinner
+        for axis in ['bottom','left']:
+          ax1.spines[axis].set_linewidth(0.3)
+
+        # Set ticks fontsize
+        plt.xticks(size=9)
+        plt.yticks(size=9)
+
+        # Set labels
+        plt.xlabel(r'Number of images $K$', fontsize=10)
+        plt.ylabel(ylabel, fontsize=10)
+
+        plt.legend(bf_order, fontsize=7, loc='upper left', frameon=False, labelspacing=0)
+
+
+plt.figure(figsize=(12,6))
 
 plt.subplot(2,3,1)
-nice_plot(opesq_bf[:,0,:,:])
+nice_plot(opesq_bf[:,0,:,:], 'PESQ [Raw MOS]')
 plt.xlabel('Number of sources')
 plt.ylabel('Raw MOS')
-plt.legend(beamformer_names, loc=2)
 
 plt.subplot(2,3,2)
-nice_plot(opesq_bf[:,1,:,:])
-plt.xlabel('Number of sources')
-plt.ylabel('MOS LQO')
-plt.legend(beamformer_names, loc=2)
+nice_plot(opesq_bf[:,1,:,:], 'PESQ [MOS LQO]')
 
 plt.subplot(2,3,3)
-nice_plot(osinr_bf)
+nice_plot(osinr_bf, 'SINR [dB]')
 plt.xlabel('Number of sources')
 plt.ylabel('output SINR')
-plt.legend(beamformer_names, loc=2)
 
 plt.subplot(2,3,4)
-nice_plot(opesq_bf[:,0,:,:] - ipesq[:,0,np.newaxis,np.newaxis])
+nice_plot(opesq_bf[:,0,:,:] - ipesq[:,0,np.newaxis,np.newaxis], 'Improvement PESQ [Raw MOS]')
 plt.xlabel('Number of sources')
 plt.ylabel('Improvement Raw MOS')
-plt.legend(beamformer_names, loc=2)
 
 plt.subplot(2,3,5)
-nice_plot(opesq_bf[:,1,:,:] - ipesq[:,1,np.newaxis,np.newaxis])
+nice_plot(opesq_bf[:,1,:,:] - ipesq[:,1,np.newaxis,np.newaxis], 'Improvement PESQ [MOS LQO]')
 plt.xlabel('Number of sources')
 plt.ylabel('Improvement MOS LQO')
-plt.legend(beamformer_names, loc=2)
 
 plt.subplot(2,3,6)
-nice_plot(osinr_bf[:,:,:] - isinr[:,np.newaxis,np.newaxis])
+nice_plot(osinr_bf[:,:,:] - isinr[:,np.newaxis,np.newaxis], 'Improvement SINR [dB]')
 plt.xlabel('Number of sources')
 plt.ylabel('Improvement SINR')
-plt.legend(beamformer_names, loc=2)
 
+plt.tight_layout(pad=0.2)
+
+plt.figure(figsize=(4,3))
+nice_plot(opesq_bf[:,0,:,:], 'PESQ [Raw MOS]', 
+        bf_order=['Rake-MaxSINR','Rake-DS','Rake-MaxUDR'])
+plt.tight_layout(pad=0.1)
 plt.savefig('figures/perceptual_quality.pdf')
 
 plt.show()
